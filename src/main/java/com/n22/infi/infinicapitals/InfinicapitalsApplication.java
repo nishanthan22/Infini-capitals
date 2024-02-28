fd92a9962ceb6dc501770136c1f55f63a39f5d4e
Nishanthan M Ravichandran <nishanthravi226@gmail.com>
2024-02-28 07:12:07 -0500

    Spring project setup

---
+++ src/main/java/com/n22/infi/infinicapitals/InfinicapitalsApplication.java
@@ -0,0 +1,13 @@
+package com.n22.infi.infinicapitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinicapitalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinicapitalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/n22/infi/infinicapitals/InfinicapitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n22.infi.infinicapitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinicapitalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
