f59627376ee4d4897c9b5e70aca6c7a974e76a8c
Nishanthan M Ravichandran <nishanthravi226@gmail.com>
2024-03-27 15:19:32 -0400

    Accounts package created

---
+++ src/main/java/com/n22/infi/infinicapitals/accounts/UserAuthentication.java
@@ -0,0 +1,11 @@
+package com.n22.infi.infinicapitals.accounts;
+
+/**
+ * Author: Nishanthan
+ * Class Description: This class consists of all the APIs and functionalities responsible for user authentication and role management services
+ */
+
+
+public class UserAuthentication {
+
+}
